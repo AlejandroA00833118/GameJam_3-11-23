1880c7f0fd02847ab3c5cee7fcb3b0223d0c85a9
Angel Garcia <102173690+AngelGarcia2207@users.noreply.github.com>
2023-11-04 13:23:15 -0600

    Sniper

---
+++ Assets/Scripts/Enemies/SniperFollow.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperFollow : MonoBehaviour
+{
+    private Transform target;
+    [SerializeField] private float speed;
+    [SerializeField] private float minimumDistance;
+    [SerializeField] private Animator enemyAnimator;
+
+    [SerializeField] private Transform gunEndPointTransform;
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private float timeBetweenShots;
+    private float nextShotTime;
+
+    private void Start(){
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        if(Time.time > nextShotTime){
+            Instantiate(projectile, gunEndPointTransform.position, gunEndPointTransform.rotation);
+            nextShotTime = Time.time + timeBetweenShots;
+        }
+        
+        if(Vector2.Distance(transform.position, target.position) > minimumDistance + 0.1f){
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
+            enemyAnimator.SetBool("IsMoving", true);
+        }
+        else if(Vector2.Distance(transform.position, target.position) < minimumDistance - 0.1f){
+            transform.position = Vector2.MoveTowards(transform.position, target.position, 0.5f * -speed * Time.fixedDeltaTime);
+            enemyAnimator.SetBool("IsMoving", true);
+        }
+        else{
+            enemyAnimator.SetBool("IsMoving", false);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
+        if(col.gameObject.tag.Equals("Bullet")){
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
