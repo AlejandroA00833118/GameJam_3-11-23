43f002365ada299054f23ed195df22a44dff1938
Angel Garcia <102173690+AngelGarcia2207@users.noreply.github.com>
2023-11-04 11:19:27 -0600

    RocketLauncher

--- Assets/Scripts/Enemies/DemolitionFollow.cs
+++ Assets/Scripts/Enemies/DemolitionFollow.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyFollow : MonoBehaviour
+public class DemolitionFollow : MonoBehaviour
 {
     private Transform target;
     [SerializeField] private float speed;
