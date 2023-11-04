23ea6a938843595b16a7e0af4f86cd7f0affa0d0
Angel Garcia <102173690+AngelGarcia2207@users.noreply.github.com>
2023-11-04 13:44:28 -0600

    Runner

---
+++ Assets/Scripts/Enemies/RunnerFollow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunnerFollow : MonoBehaviour
+{
+    private Transform target;
+    [SerializeField] private float speed;
+    [SerializeField] private float minimumDistance;
+    [SerializeField] private Animator enemyAnimator;
+
+    private void Start(){
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        if(Vector2.Distance(transform.position, target.position) < minimumDistance - 0.2f){
+            transform.position = Vector2.MoveTowards(transform.position, target.position, 0.5f * -speed * Time.fixedDeltaTime);
+            enemyAnimator.SetBool("IsMoving", true);
+        }
+        else{
+            enemyAnimator.SetBool("IsMoving", false);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
+        if(col.gameObject.tag.Equals("Bullet")){
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
