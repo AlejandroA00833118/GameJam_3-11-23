2a069f8d2ed008772f827f65144eb272c465d3b5
AlejandroA00833118 <a00833118@itesm.mx>
2023-11-04 13:26:42 -0600

    Controladores de sprite y animator

---
+++ Assets/Scripts/WeaponControllerAnimator.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponControllerAnimator : MonoBehaviour
+{
+    public RuntimeAnimatorController[] animatorControllers;
+
+    void Start() {
+        ChangeAnimatorController(animatorControllers[1]);
+    }
+    
+    void ChangeAnimatorController(RuntimeAnimatorController newAnimatorController)
+    {
+        Animator animator = GetComponent<Animator>();
+        
+        if (animator != null && newAnimatorController != null)
+        {
+            animator.runtimeAnimatorController = newAnimatorController;
+        }
+    }
+}
