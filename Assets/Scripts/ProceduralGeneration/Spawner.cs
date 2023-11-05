ff01630be9dce1d2b51679fa8fbcd02a8ad02ba0
AlejandroA00833118 <a00833118@itesm.mx>
2023-11-05 13:56:43 -0700

    Spawn de probs

--- Assets/Scripts/ProceduralGeneration/Spawner.cs
+++ Assets/Scripts/ProceduralGeneration/Spawner.cs
@@ -6,7 +6,7 @@ public class Spawner : MonoBehaviour
 {
     public GameObject[] props;
 
-    public int propsMaxItems = 15;
+    public int propsMaxItems = 10;
 
     public float waitTime = 4f;
 
@@ -16,11 +16,11 @@ public class Spawner : MonoBehaviour
     }
 
     IEnumerator SpawnProps() {
-        int randomAmount = Random.Range(10, propsMaxItems);
+        int randomAmount = Random.Range(5, propsMaxItems);
         for (int i = 0; i < randomAmount; i++) {
             int randomIndex = Random.Range(0, props.Length);
             float offsetX = Random.Range(-4f, 4f);
-            float offsetY = Random.Range(-4f, 4f);
+            float offsetY = Random.Range(-3.5f, 3.5f);
             Vector3 offset = new Vector3(offsetX, offsetY, 0);
             Instantiate(props[randomIndex], transform.position + offset, Quaternion.identity);
         }
