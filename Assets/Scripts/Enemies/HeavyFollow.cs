a8f5e042da3f1355e47ba7c6121890e73ff62e92
Angel Garcia <102173690+AngelGarcia2207@users.noreply.github.com>
2023-11-05 16:57:44 -0600

    Deteccion

--- Assets/Scripts/Enemies/HeavyFollow.cs
+++ Assets/Scripts/Enemies/HeavyFollow.cs
@@ -15,30 +15,45 @@ public class HeavyFollow : MonoBehaviour
     private float nextShotTime;
     public bool moving;
 
+    [SerializeField] private float detectionDistance;
+    [SerializeField] private float followDistance;
+    [SerializeField] private bool following = false;
+    [SerializeField] private GameObject gun;
+
     private void Start(){
         target = GameObject.FindWithTag("Player").transform;
     }
 
     private void Update()
     {  
-        if(Vector2.Distance(transform.position, target.position) > minimumDistance + 0.4f){
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
-            enemyAnimator.SetBool("IsMoving", true);
-            moving = true;
-        }
-        else if(Vector2.Distance(transform.position, target.position) < minimumDistance - 0.4f){
-            transform.position = Vector2.MoveTowards(transform.position, target.position, 0.5f * -speed * Time.fixedDeltaTime);
-            enemyAnimator.SetBool("IsMoving", true);
-            moving = true;
-        }
-        else{
-            enemyAnimator.SetBool("IsMoving", false);
-            moving = false;
-        }
+        if(Vector2.Distance(transform.position, target.position) < followDistance && following == true){
+            if(Vector2.Distance(transform.position, target.position) > minimumDistance + 0.4f){
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
+                enemyAnimator.SetBool("IsMoving", true);
+                moving = true;
+            }
+            else if(Vector2.Distance(transform.position, target.position) < minimumDistance - 0.4f){
+                transform.position = Vector2.MoveTowards(transform.position, target.position, 0.5f * -speed * Time.fixedDeltaTime);
+                enemyAnimator.SetBool("IsMoving", true);
+                moving = true;
+            }
+            else{
+                enemyAnimator.SetBool("IsMoving", false);
+                moving = false;
+            }
 
-        if(Time.time > nextShotTime && moving == false){
-            Instantiate(projectile, gunEndPointTransform.position, gunEndPointTransform.rotation);
-            nextShotTime = Time.time + timeBetweenShots;
+            if(Time.time > nextShotTime && moving == false){
+                Instantiate(projectile, gunEndPointTransform.position, gunEndPointTransform.rotation);
+                nextShotTime = Time.time + timeBetweenShots;
+            }
+        }
+        else if(Vector2.Distance(transform.position, target.position) < detectionDistance && following == false){
+            following = true;
+            gun.SetActive(true);
+        }
+        else if(Vector2.Distance(transform.position, target.position) > followDistance && Vector2.Distance(transform.position, target.position) > detectionDistance){
+            following = false;
+            gun.SetActive(false);
         }
     }
 
--- Assets/Scripts/Enemies/RunnerFollow.cs
+++ Assets/Scripts/Enemies/RunnerFollow.cs
@@ -9,19 +9,32 @@ public class RunnerFollow : MonoBehaviour
     [SerializeField] private float minimumDistance;
     [SerializeField] private Animator enemyAnimator;
 
+    [SerializeField] private float detectionDistance;
+    [SerializeField] private float followDistance;
+    [SerializeField] private bool following = false;
+
     private void Start(){
         target = GameObject.FindWithTag("Player").transform;
     }
 
     private void Update()
     {
-        if(Vector2.Distance(transform.position, target.position) < minimumDistance - 0.2f){
-            transform.position = Vector2.MoveTowards(transform.position, target.position, 0.5f * -speed * Time.fixedDeltaTime);
-            enemyAnimator.SetBool("IsMoving", true);
+        if(Vector2.Distance(transform.position, target.position) < followDistance && following == true){
+            if(Vector2.Distance(transform.position, target.position) < minimumDistance - 0.2f){
+                transform.position = Vector2.MoveTowards(transform.position, target.position, 0.5f * -speed * Time.fixedDeltaTime);
+                enemyAnimator.SetBool("IsMoving", true);
+            }
+            else{
+                enemyAnimator.SetBool("IsMoving", false);
+            }
         }
-        else{
-            enemyAnimator.SetBool("IsMoving", false);
+        else if(Vector2.Distance(transform.position, target.position) < detectionDistance && following == false){
+            following = true;
         }
+        else if(Vector2.Distance(transform.position, target.position) > followDistance && Vector2.Distance(transform.position, target.position) > detectionDistance){
+            following = false;
+        }
+        
     }
 
     void OnCollisionEnter2D(Collision2D col){
